8415324f668ff45c68b5c92c1602e458476c1ea1
LuisFernando-Healy <garcialuismeza777@gmail.com>
2025-11-08 16:47:33 -0600

    puse un sout xd

---
+++ tradtok/AnalisisLexico.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class AnalisisLexico {
+    
+private final String abecedario = "abcdefghijlmnñopqrstuvyz ";
+
+    public ArrayList<Token> analizar(String palabra) throws Exception{
+
+       ArrayList<Token> tokens = new ArrayList<>();
+
+        for (int i = 0; i < palabra.length(); i++) {
+             char letra = palabra.charAt(i);
+            System.out.println("act");
+            // 2 Verificamos si el carácter está en nuestro abecedario
+
+            char validar = Character.toLowerCase(letra);
+            if(!abecedario.contains(Character.toString(validar))){
+                throw new Exception("caracter invalido");
+           
+            }
+
+            // agregar tokens ..
+            tokens.add(new Token(validar));
+        }
+        
+        return tokens;
+
+    }
+
+  
+            
+
+}
